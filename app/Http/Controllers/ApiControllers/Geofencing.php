37833de920e5d5ad9456c5af567c08f06eeb4474
progmaster001 <a.rehman5110@gmail.com>
2023-01-04 11:26:50 +0500

    update

--- app/Http/Controllers/ApiControllers/Geofencing.php
+++ app/Http/Controllers/ApiControllers/Geofencing.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\ApiControllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\geom;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,14 +13,20 @@ class Geofencing extends Controller
 
     public function userLocation(Request $request)
     {
+       if(geom::where('user_name',$request->username)->first()){
+
        
         $log = $request->log ;
         $lat = $request->lat;
         $user = $request->username;
         $result = DB::select("select st_intersects(st_geomfromtext('POINT('||$lat||' '||$log||')',4326),geom)  
-        from client_geoms  where id=1");
+        from client_geoms  where user_name='$user'");
 
         return $result[0];
+       }
+       else{
+        return response()->json(['message'=>'user not found']);
+       }
         
     }
 }
