d220d71034e3821b65cc4012b57a1615650539be
progmaster001 <a.rehman5110@gmail.com>
2023-01-04 17:48:43 +0500

    update

--- app/Http/Controllers/ApiControllers/Geofencing.php
+++ app/Http/Controllers/ApiControllers/Geofencing.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\ApiControllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Client;
 use App\Models\geom;
+use App\Models\maid;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -13,14 +15,16 @@ class Geofencing extends Controller
 
     public function userLocation(Request $request)
     {
-       if(geom::where('user_name',$request->username)->first()){
+    
+        $maid = maid::where('user_name',$request->username)->first();
+       if($maid){
 
        
         $log = (float)$request->log ;
         $lat = (float)$request->lat;
-        $user = $request->username;
+         $user = $maid->client_boundary_id;
         $result = DB::select("select st_intersects(st_geomfromtext('POINT('||$lat||' '||$log||')',4326),geom)  
-        from client_geoms  where user_name='$user'");
+        from client_geoms  where id='$user'");
 
         return $result[0];
        }
