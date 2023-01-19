b94b5e097fd9406d1e57c882c6837286faf7d84c
progmaster001 <a.rehman5110@gmail.com>
2023-01-19 19:10:53 +0500

    create track user api

--- app/Http/Controllers/ApiControllers/Geofencing.php
+++ app/Http/Controllers/ApiControllers/Geofencing.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Client;
 use App\Models\geom;
 use App\Models\maid;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -15,53 +16,51 @@ class Geofencing extends Controller
 
     public function userLocation(Request $request)
     {
-    
-        $maid = maid::where('user_name',$request->username)->first();
-       if($maid){
+        $maid = maid::where('user_name', $request->username)->first();
+        if ($maid) {
+            $log = (float) $request->log;
+            $lat = (float) $request->lat;
+            $user = $maid->client_boundary_id;
+            $query = "select st_intersects(st_geomfromtext('POINT('||$lat||' '||$log||')',4326),geom)
+        from client_geoms  where id='$user'";
+            if ($user == '') {
+                return response()->json(['message' => 'no boundary found against this user']);
+            } else {
+                //echo  $query;
+                //exit();
+                $result = DB::select($query);
 
-       
-        $log = (float)$request->log ;
-        $lat = (float)$request->lat;
-         $user = $maid->client_boundary_id;
-        $query="select st_intersects(st_geomfromtext('POINT('||$lat||' '||$log||')',4326),geom)  
-        from client_geoms  where id='$user'"; 
-        if($user==''){
-            return response()->json(['message'=>'no boundary found against this user']);
-        }else{
-        //echo  $query;
-        //exit();
-        $result = DB::select($query);
-
-        return $result[0];
+                return $result[0];
+            }
+        } else {
+            return response()->json(['message' => 'user not found']);
         }
-       }
-       else{
-        return response()->json(['message'=>'user not found']);
-       }
-        
     }
-
-
 
     public function TrackUserLocation(Request $req)
     {
+        if (!$req->has('user_id') || $req->user_id == '') {
+            return response()->json(['status' => 'failed', 'message' => 'user id is required']);
+        }
+        try {
+            $lat_log = DB::select("Select st_x(geom) as lon, st_y(geom) as lat from tbl_user_activity where user_id = $req->user_id");
+            if (!$lat_log) {
+                return response()->json(['status' => 'failed', 'message' => 'latitude or longitude not found']);
+            }
 
-        $lat_log = DB::select("Select st_x(geom) as lon, st_y(geom) as lat from tbl_user_activity where user_id = $req->user_id");
-        if(!$lat_log){
-            return response()->json(['status'=>'failed','message'=>'latitude or longitude not found']);
-        }
-        
-        $user = DB::select("select client_boundary_id from tbl_user where id = $req->user_id");
-        if(!$user){
-            return response()->json(['status'=>'failed','message'=>'client boundary id not found']);
-        }
-     
-        $boundary = DB::select("select st_intersects(st_geomfromtext('POINT('||".$lat_log[0]->lat."||' '||".$lat_log[0]->lon."||')',4326),geom) from client_geoms where id='".$user[0]->client_boundary_id."'");
+            $user = DB::select("select client_boundary_id from tbl_user where id = $req->user_id");
+            if (!$user) {
+                return response()->json(['status' => 'failed', 'message' => 'client boundary id not found']);
+            }
 
-        if(!$boundary){
-            return response()->json(['status'=>'failed','message'=>'client geom not found']);
+            $boundary = DB::select("select st_intersects(st_geomfromtext('POINT('||" . $lat_log[0]->lat . "||' '||" . $lat_log[0]->lon . "||')',4326),geom) from client_geoms where id='" . $user[0]->client_boundary_id . "'");
+
+            if (!$boundary) {
+                return response()->json(['status' => 'failed', 'message' => 'client geom not found']);
+            }
+            return response()->json(['status' => 'success', 'data' => $boundary[0]]);
+        } catch (Exception $e) {
+            return response()->json(['status' => 'failed', 'message' => 'something is worg......']);
         }
-        return response()->json(['status'=>'success','data'=>$boundary[0]]);
-        
     }
 }
